ec39da1f8db62208f832451e1fb7bbea317064fd
lpaube <lpaube@hotmail.com>
2021-09-27 13:33:02 -0400

    feature: deleting and killing all zombies

--- module01/ex01/main.cpp
+++ module01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laube <louis-philippe.aube@hotmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 22:16:20 by laube             #+#    #+#             */
-/*   Updated: 2021/09/27 13:27:33 by laube            ###   ########.fr       */
+/*   Updated: 2021/09/27 13:32:49 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	main(void)
 	int	N;
 
 	N = 10;
-	
 	zombies = zombieHorde(N, "what");
 	for (int i = 0; i < N; i++)
 	{
 		zombies[i].announce();
 	}
+	delete[] zombies;
 	return (0);
 }
