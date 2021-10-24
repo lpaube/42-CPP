8a40e164012b387cb5f6aa28316122d3a5777641
LPAube <lpaube@hotmail.com>
2021-10-24 01:52:32 -0400

    corrections

--- module01/ex05/Karen.cpp
+++ module01/ex05/Karen.cpp
@@ -1,13 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Account.cpp                                        :+:      :+:    :+:   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: laube <laube@student.42quebec.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/19 00:22:48 by laube             #+#    #+#             */
-/*   Updated: 2021/10/19 00:24:01 by laube            ###   ########.fr       */
+/*   Created: 2021/10/23 11:27:59 by laube             #+#    #+#             */
+/*   Updated: 2021/10/23 11:30:24 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+class Karen {
+    
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    
+    public:
+        void complain(std::string level);
+}
