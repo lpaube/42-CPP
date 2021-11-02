da97bb30746dde708afb96aebf066668094acaa4
LPAube <lpaube@hotmail.com>
2021-11-02 01:42:07 -0400

    working on exceptions

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -3,10 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: laube <laube@student.42quebec.com>         +#+  +:+       +#+        */
+/*   By: laube <louis-philippe.aube@hotmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 16:47:05 by laube             #+#    #+#             */
-/*   Updated: 2021/10/11 16:47:06 by laube            ###   ########.fr       */
+/*   Updated: 2021/10/30 17:59:18 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {
+    name = "Generic Bureaucrat"
+    grade = 150;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) {
+    this->name = name;
+    this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& src) {
+    name = src.name;
+    grade = src.grade;
+}
+
+Bureaucrat::operator=(const Bureaucrat& rhs) {
+    if (this == rhs) return (*this);
+    name = rhs.name;
+    grade = src.grade;
+    return (*this);
+}
+
+std::string Bureaucrat::getName() {
+    return (name);
+}
+
+int Bureaucrat::getGrade() {
+    return (grade);
+}
+
+void Bureaucrat::incGrade() {
+    grade--;
+}
+
+void Bureaucrat::decGrade() {
+    grade++;
+}
+
+std::ostream& operator<<(std::ostream& op, Bureaucrat const& rhs) {
+    op << rhs.name << ", bureaucrat grade " << rhs.grade << "." << std::endl;
+    return (op);
+}
--- module05/ex00/Bureaucrat.hpp
+++ module05/ex00/Bureaucrat.hpp
@@ -6,7 +6,28 @@
 /*   By: laube <laube@student.42quebec.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 16:46:34 by laube             #+#    #+#             */
-/*   Updated: 2021/10/11 16:47:09 by laube            ###   ########.fr       */
+/*   Updated: 2021/10/31 01:14:18 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+class Bureaucrat {
+    private:
+        const std::string name;
+        int grade;
+
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string name, int grade);
+        Bureaucrat(const Bureaucrat& src);
+        ~Bureaucrat();
+        Bureaucrat& operator=(const Bureaucrat& rhs);
+
+        const std::string getName();
+        int getGrade();
+        void incGrade();
+        void decGrade();
+};
+
+std::ostream& operator<<(std::ostream& op, Bureaucrat const& rhs);
