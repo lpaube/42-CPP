1df31a3a91985ac1b04075db852c4cfbebee0d07
LPaube <lpaube@hotmail.com>
2021-11-24 13:06:54 -0500

    created required template functions and tests

---
+++ module07/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laube <marvin@42quebec.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 13:00:32 by laube             #+#    #+#             */
+/*   Updated: 2021/11/24 13:06:34 by laube            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "template.hpp"
+
+#include <iostream>
+
+int main( void ) {
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return 0;
+}
---
+++ module07/ex00/template.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   template.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laube <marvin@42quebec.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 12:54:21 by laube             #+#    #+#             */
+/*   Updated: 2021/11/24 13:06:19 by laube            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template<typename T>
+void swap(T& x, T& y) {
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template<typename T>
+const T	max(const T& x, const T& y) {
+	return (x >= y ? x : y);
+}
+
+template<typename T>
+const T min(const T& x, const T& y) {
+	return (x <=y ? x : y);
+}
