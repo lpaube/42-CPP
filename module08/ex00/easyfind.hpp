f8bdc11f64970e323484287338c8fe568b524f84
LPaube <lpaube@hotmail.com>
2021-11-29 15:38:30 -0500

    now using STL algorithm

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,25 +6,22 @@
 /*   By: laube <marvin@42quebec.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 17:41:32 by laube             #+#    #+#             */
-/*   Updated: 2021/11/29 12:18:56 by laube            ###   ########.fr       */
+/*   Updated: 2021/11/29 15:37:26 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stdexcept>
-#include <exception>
+#include <algorithm>
 
 template<typename T>
-typename T::iterator easyfind(T& num_container, int y) {
+typename T::iterator easyfind(T& num_container, int num) {
 
-	typename T::iterator it;
+	typename T::iterator it = num_container.begin();
 	typename T::iterator ite = num_container.end();
 
-	for (it = num_container.begin(); it != ite; it++) {
-		if (*it == y)
-        {
-			return it;
-        }
-	}
+	typename T::iterator res = std::find(it, ite, num);
+	if (res != ite)
+		return res;
 	throw std::invalid_argument("Number not found");
 }
